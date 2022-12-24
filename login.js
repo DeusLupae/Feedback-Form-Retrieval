a7f7be80175ee51c5bfecbe09ce248bfcbd7261b
DeusLupae <121142272+DeusLupae@users.noreply.github.com>
2022-12-23 20:47:34 -0600

    Website Creation

---
+++ login.js
@@ -0,0 +1,14 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+
+    if(username=="CoopBlakley1234"&& password=="matbyf-3keqho-wejCug")
+    {
+        open(http://cogitatorius.blue/formdata/herfy6-dazcer-wihjyF)
+    }
+    else
+    {
+        alert("Incorrect Username/Password.");
+    }
+}
