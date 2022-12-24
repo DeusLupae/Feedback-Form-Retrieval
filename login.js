5d5d70fe34797ea2ab725cf2a441363fd3af1b34
DeusLupae <121142272+DeusLupae@users.noreply.github.com>
2022-12-23 21:18:57 -0600

    Java Fix 3

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function validate() {  
     var username = document.getElementById("username");  
     var password = document.getElementById("password");  
-    if(username=="CoopBlakley1234"&& password=="matbyf-3keqho-wejCug")  
+    if(username = "CoopBlakley1234" & password = "matbyf-3keqho-wejCug")  
     {   
       open(http://herfy6-dazcer-wihjyf.cogitatorius.blue);  
     } else {  
