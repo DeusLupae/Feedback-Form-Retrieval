1ac08c9376f35b81131dba4b25955b021013b3ef
DeusLupae <121142272+DeusLupae@users.noreply.github.com>
2022-12-23 21:28:02 -0600

    iAUDGF

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 function validate() {  
     var username = document.getElementById("username");  
     var password = document.getElementById("password");  
-    if(username == "CoopBlakley1234" && password == "matbyf-3keqho-wejCug")  
+    if(username == "CoopBlakley1234")  
     {   
-      window.open(https://www.youtube.com);  
+      open(https://www.youtube.com);  
     } else {  
       alert("Username/Password Incorrect. Try again.");  
     }  
